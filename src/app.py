6d73fe2425a679db031d9768f24b6f9490e85c3e
Akitsuyoshi <ka090824@gmail.com>
2022-02-14 06:29:19 +0000

    Add linear and binary search

--- src/app.py
+++ src/app.py
@@ -1,2 +1,37 @@
+from typing import Union
+
 # %%
-print("Hellow world")
+
+
+def linear_search(arr: list[int], val: int) -> Union[int, None]:
+    for elm in arr:
+        if elm == val:
+            return val
+        elif elm > val:
+            break
+    return None
+
+
+print(linear_search([3, 17, 22, 80, 220], 25))
+
+# %%
+
+
+def binary_search(arr: list[int], val: int) -> Union[int, None]:
+    lower_bound = 0
+    upper_bound = len(arr) - 1
+
+    while lower_bound <= upper_bound:
+        mid_point = (upper_bound + lower_bound) // 2
+        val_at_mid_point = arr[mid_point]
+
+        if val == val_at_mid_point:
+            return mid_point
+        elif val < val_at_mid_point:
+            upper_bound = mid_point - 1
+        elif val > val_at_mid_point:
+            lower_bound = mid_point + 1
+
+    return None
+
+# %%
